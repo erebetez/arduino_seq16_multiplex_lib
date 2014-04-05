06a5b7d1f633ce98da6d22f3beaa7fb0753c2916
Etienne Rebetez <etienne.rebetez@oberwallis.ch>
2014-04-05 21:15:52 +0200

    add buttons.

--- multiplex_blink/multiplex_blink.ino
+++ multiplex_blink/multiplex_blink.ino
@@ -2,25 +2,202 @@
   Driver for two Multiplexer driving leds.
  */
 
-int m_led_inhibit = 2;
-int m_led_a = 3;
-int m_led_b = 4;
-int m_led_c = 5;
+#define TYPE_LED 0
+
+#define TYPE_BUTTON 1
 
 
+const int m_led_inhibit = 2;
+const int m_led_a = 3;
+const int m_led_b = 4;
+const int m_led_c = 5;
 
+const int m_btn_a = 6;
+const int m_btn_b = 7;
+const int m_btn_c = 8;
+const int m_btn_1 = 10;
+const int m_btn_2 = 11;
+
+const int m_btn_f = 12;
+
+int m_current_button; // TODO make array;
+int m_last_button;
 
 void setup() {
+  Serial.begin(9600);
+    
+  Serial.println("Hello!");
+    
+  init_pins();
+
+//  test_init();
+ 
+  ledOn(1);
+  
+  
+}
+
+
+void loop() {
+
+//  m_last_button = m_current_button;
+  readButtons();
+  
+//  if(m_last_button != m_current_button){
+//    ledOn(m_current_button);
+//  }
+
+  
+//   Serial.println(m_current_button);
+   delay(1000);
+}
+
+
+void init_pins(){
   // initialize the digital pin as an output.
   pinMode(m_led_inhibit, OUTPUT); 
   pinMode(m_led_a, OUTPUT);
   pinMode(m_led_b, OUTPUT);
   pinMode(m_led_c, OUTPUT);
 
+  pinMode(m_btn_a, OUTPUT);
+  pinMode(m_btn_b, OUTPUT);
+  pinMode(m_btn_c, OUTPUT);  
+  
+  pinMode(m_btn_1, INPUT);
+  pinMode(m_btn_2, INPUT);
+  
+  pinMode(m_btn_f, INPUT);
+}
+
+void keepLedOn(int num, int num2, int time){
+   for(int i = 0; i < (time / 2); ++i){
+     ledOn(num);
+     delay(1);
+     ledOn(num2);
+     delay(1);
+   }
 }
 
 
-void loop() {
+void ledOn(int num){
+
+  if(num <= 8){
+    digitalWrite(m_led_inhibit, LOW);
+  } else {
+    num = num - 8;
+    digitalWrite(m_led_inhibit, HIGH);
+  }
+
+  activateMuliplex(--num, TYPE_LED);
+}
+
+
+
+void readButtons(){
+  int buttonState = LOW;
+  
+  
+  Serial.println("__ readButtons __");
+  buttonState = digitalRead(m_btn_f);
+  Serial.print("Function:\t");
+  Serial.print(buttonState);
+  Serial.println("");
+     
+  for(int i = 0; i < 8; ++i){
+    activateMuliplex(i, TYPE_BUTTON);
+    
+     Serial.print("Buttons 1-8:\t");
+     Serial.print(i+1);
+     Serial.print("\t");
+     buttonState = digitalRead(m_btn_1);
+      
+     Serial.print(buttonState);
+     Serial.println("");
+      
+      if(buttonState == HIGH){
+        m_current_button = (i+1);
+//             Serial.println(m_current_button);
+        ledOn(m_current_button);
+        m_current_button = (i + 1);  
+        
+        return; 
+      }
+
+     Serial.print("Buttons 9-16:\t");      
+     Serial.print(i+9);
+     Serial.print("\t");     
+     buttonState = digitalRead(m_btn_2); 
+      
+     Serial.print(buttonState);
+     Serial.println("");
+        
+     if(buttonState == HIGH){
+          m_current_button = (i+9);
+//             Serial.println(m_current_button);
+          ledOn(m_current_button);
+          m_current_button = (i + 9); // seccond 8 - 16;   
+           
+        return;   
+      }
+  }
+  
+  
+}
+
+
+void activateMuliplex(int i, int type){
+   switch (i) {
+    case 0:
+      writeMultiplex(LOW, LOW, LOW, type);
+      break;
+    case 1:
+      writeMultiplex(HIGH, LOW, LOW, type);
+      break;
+    case 2:
+      writeMultiplex(LOW, HIGH, LOW, type);
+      break;
+    case 3:
+      writeMultiplex(HIGH, HIGH, LOW, type);
+      break;
+    case 4:
+      writeMultiplex(LOW, LOW, HIGH, type);
+      break;
+    case 5:
+      writeMultiplex(HIGH, LOW, HIGH, type);
+      break;
+    case 6:
+      writeMultiplex(LOW, HIGH, HIGH, type);
+      break;
+    case 7:
+      writeMultiplex(HIGH, HIGH, HIGH, type);
+      break;
+  } 
+}
+
+void writeMultiplex(int a, int b, int c, int type) {
+  if(type == TYPE_LED){
+    writeMultiplexLed(a, b, c);
+  }
+  if(type == TYPE_BUTTON){
+    writeMultiplexBtn(a, b, c);
+  }
+}
+
+void writeMultiplexLed(int a, int b, int c) {
+  digitalWrite(m_led_a, a);
+  digitalWrite(m_led_b, b);
+  digitalWrite(m_led_c, c);
+}
+
+void writeMultiplexBtn(int a, int b, int c) {
+  digitalWrite(m_btn_a, a);
+  digitalWrite(m_btn_b, b);
+  digitalWrite(m_btn_c, c);
+}
+
+
+void test_init(){
   // Test 1
   ledOn(1);
   delay(800);
@@ -60,59 +237,5 @@ void loop() {
       keepLedOn(3 , 11, 100);
   }
   ledOn(4);
-  delay(200);
-
-}
-
-void keepLedOn(int num, int num2, int time){
-   for(int i = 0; i < (time / 2); ++i){
-     ledOn(num);
-     delay(1);
-     ledOn(num2);
-     delay(1);
-   }
-}
-
-
-void ledOn(int num){
-
-  if(num <= 8){
-    digitalWrite(m_led_inhibit, LOW);
-  } else {
-    num = num - 8;
-    digitalWrite(m_led_inhibit, HIGH);
-  }
-
-  switch (num) {
-    case 1:
-      writeMultiplex(LOW, LOW, LOW);
-      break;
-    case 2:
-      writeMultiplex(HIGH, LOW, LOW);
-      break;
-    case 3:
-      writeMultiplex(LOW, HIGH, LOW);
-      break;
-    case 4:
-      writeMultiplex(HIGH, HIGH, LOW);
-      break;
-    case 5:
-      writeMultiplex(LOW, LOW, HIGH);
-      break;
-    case 6:
-      writeMultiplex(HIGH, LOW, HIGH);
-      break;
-    case 7:
-      writeMultiplex(LOW, HIGH, HIGH);
-      break;
-    case 8:
-      writeMultiplex(HIGH, HIGH, HIGH);
-      break;
-  }
-}
-
-void writeMultiplex(int a, int b, int c) {
-  digitalWrite(m_led_a, a);
-  digitalWrite(m_led_b, b);
-  digitalWrite(m_led_c, c);
+  delay(200);  
 }
